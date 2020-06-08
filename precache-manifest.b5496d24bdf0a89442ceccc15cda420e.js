da9d7c96294689bb1dd882c94f15494ff90de4a1
Daniel Aguirre <danter1@gmail.com>
2020-06-08 00:34:08 -0500

    Updates

---
+++ precache-manifest.b5496d24bdf0a89442ceccc15cda420e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06179ff807c6af72c04a20e967b597b0",
+    "url": "/DanielAguirre/flags-project.git/index.html"
+  },
+  {
+    "revision": "be4db8c9f26150a9bfe3",
+    "url": "/DanielAguirre/flags-project.git/static/css/main.b6a1c437.chunk.css"
+  },
+  {
+    "revision": "87c50f52c00417f9003a",
+    "url": "/DanielAguirre/flags-project.git/static/js/2.a69d18f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DanielAguirre/flags-project.git/static/js/2.a69d18f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4db8c9f26150a9bfe3",
+    "url": "/DanielAguirre/flags-project.git/static/js/main.3ea12798.chunk.js"
+  },
+  {
+    "revision": "76722edf010f616b5d94",
+    "url": "/DanielAguirre/flags-project.git/static/js/runtime-main.5325c4a7.js"
+  }
+]);
